3598a4044b13e9783d579c18bfdcb0c1835c21b2
abbiyudha <abbiyudha14@gmail.com>
2022-03-28 18:13:36 +0700

    rent

--- delivery/handler/rent/rent.go
+++ delivery/handler/rent/rent.go
@@ -1,1 +1,59 @@
 package rent
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"rentBook/delivery/helper"
+	"rentBook/delivery/middlewares"
+	"rentBook/entities"
+	_rentUseCase "rentBook/usecase/rent"
+)
+
+type RentHandler struct {
+	rentUseCase _rentUseCase.RentUseCaseInterface
+}
+
+func NewRentHandler(rentUseCase _rentUseCase.RentUseCaseInterface) *RentHandler {
+	return &RentHandler{
+		rentUseCase: rentUseCase,
+	}
+}
+
+func (rh *RentHandler) GetAllHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		rents, err := rh.rentUseCase.GetAll()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("failed to fetch data"))
+		}
+		return c.JSON(http.StatusOK, helper.ResponseSuccess("success get all rents", rents))
+	}
+}
+
+func (rh *RentHandler) CreateRentHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var rent entities.Rent
+
+		c.Bind(&rent)
+		id := middlewares.ExtractToken(c)
+		rent.UserID = uint(id)
+		err := rh.rentUseCase.CreateRent(rent)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("failed to rent book"))
+		}
+		return c.JSON(http.StatusOK, helper.ResponseSuccess("success create rents", rent))
+	}
+}
+func (rh *RentHandler) ReturnBookHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var rent entities.Rent
+
+		c.Bind(&rent)
+		id := middlewares.ExtractToken(c)
+		rent.UserID = uint(id)
+		err := rh.rentUseCase.ReturnBook(rent)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, helper.ResponseFailed("failed to return book"))
+		}
+		return c.JSON(http.StatusOK, helper.ResponseSuccess("success return book", rent))
+	}
+}
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	_rentHandler "rentBook/delivery/handler/rent"
+	_rentRepository "rentBook/repository/rent"
+	_rentUseCase "rentBook/usecase/rent"
 
 	"rentBook/config"
 	_userHandler "rentBook/delivery/handler/user"
@@ -32,12 +35,16 @@ func main() {
 	bookUse := bookUseCase.NewBookUseCase(bookRepo)
 	bookHand := bookHandler.NewBookHandler(bookUse)
 
+	rentRepo := _rentRepository.NewRentRepository(db)
+	rentUse := _rentUseCase.NewRentUseCase(rentRepo, bookRepo)
+	rentHand := _rentHandler.NewRentHandler(rentUse)
+
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(_middlewares.CustomLogger())
 
-	_routes.RegisterPath(e, userHandler, bookHand)
+	_routes.RegisterPath(e, userHandler, bookHand, rentHand)
 
-	_routes.RegisterPath(e, userHandler, bookHand)
+	_routes.RegisterPath(e, userHandler, bookHand, rentHand)
 	log.Fatal(e.Start(fmt.Sprintf(":%d", configs.Port)))
 }
