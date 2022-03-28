5ec5f2d7619cce1f83fa33c7fa41fdcec612054e
abbiyudha <abbiyudha14@gmail.com>
2022-03-28 18:52:26 +0700

    update

--- delivery/routes/router.go
+++ delivery/routes/router.go
@@ -18,7 +18,7 @@ func RegisterPath(e *echo.Echo, uh *_userHandler.UserHandler, bh *book.BookHandl
 	e.GET("/users", uh.GetAllHandler(), _middlewares.JWTMiddleware())
 	e.GET("/users/profile", uh.GetUserById(), _middlewares.JWTMiddleware())
 	e.POST("/users", uh.CreateUser())
-	e.DELETE("/users/:id", uh.DeleteUser(), _middlewares.JWTMiddleware())
+	e.DELETE("/users", uh.DeleteUser(), _middlewares.JWTMiddleware())
 	e.PUT("/users/:id", uh.UpdateUser(), _middlewares.JWTMiddleware())
 
 	e.GET("/books", bh.GetAllHandler())
