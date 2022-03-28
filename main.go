124a8028eec878ecb0c72c4c37da36b10e13a29d
abbiyudha <abbiyudha14@gmail.com>
2022-03-28 17:49:56 +0700

    user

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+
+	"rentBook/config"
+	_userHandler "rentBook/delivery/handler/user"
+	_userRepository "rentBook/repository/user"
+	_userUseCase "rentBook/usecase/user"
+
+	_middlewares "rentBook/delivery/middlewares"
+	_routes "rentBook/delivery/routes"
+	_utils "rentBook/utils"
 )
 
 func main() {
 	configs := config.GetConfig()
 	db := _utils.InitDB(configs)
 
+	userRepo := _userRepository.NewUserRepository(db)
+	userUseCase := _userUseCase.NewUserUseCase(userRepo)
+	userHandler := _userHandler.NewUserHandler(userUseCase)
+
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(_middlewares.CustomLogger())
+
+	_routes.RegisterPath(e, userHandler)
+
+	_routes.RegisterPath(e, userHandler)
+	log.Fatal(e.Start(fmt.Sprintf(":%d", configs.Port)))
 }
--- usecase/user/user.go
+++ usecase/user/user.go
@@ -1,1 +1,42 @@
 package user
+
+import (
+	_entities "rentBook/entities"
+	_userRepository "rentBook/repository/user"
+)
+
+type UserUseCase struct {
+	UserRepository _userRepository.UserRepositoryInterface
+}
+
+func NewUserUseCase(userRepo _userRepository.UserRepositoryInterface) UserUseCaseInterface {
+	return &UserUseCase{
+		UserRepository: userRepo,
+	}
+
+}
+
+func (uuc *UserUseCase) GetAll() ([]_entities.User, error) {
+	users, err := uuc.UserRepository.GetAll()
+	return users, err
+}
+
+func (uuc *UserUseCase) GetUserById(id int) (_entities.User, error) {
+	user, err := uuc.UserRepository.GetUserById(id)
+	return user, err
+}
+
+func (uuc *UserUseCase) CreateUser(user _entities.User) error {
+	err := uuc.UserRepository.CreateUser(user)
+	return err
+}
+
+func (uuc *UserUseCase) DeleteUser(id int) error {
+	err := uuc.UserRepository.DeleteUser(id)
+	return err
+}
+
+func (uuc *UserUseCase) UpdateUser(id int, user _entities.User) error {
+	err := uuc.UserRepository.UpdateUser(id, user)
+	return err
+}
